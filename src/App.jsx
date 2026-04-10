11072912b72b74f1ddc6fa7c94f2f6776f52b326
Ibso77 <ibrohimjonsobirov68@gmail.com>
2026-04-10 19:51:48 +0500

    Product files added

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import Navbar from './components/navbar';
-import Hero from './components/hero';
-import ProductSlider from './components/productlist';
-import Vet from './components/vet';
-import ArticlesSection from './components/interest';
-import BrandsSection from './components/brend';
-import Textinfo from './components/text';
-import Footer from './components/footer';
+import { Routes, Route } from 'react-router-dom';
+import MainLayout from './layout/MainLayout';
+import Home from './pages/Home';
 
 const App = () => {
     return (
-        <div>
-            <Navbar/>
-            <Hero/>
-            <ProductSlider/>
-            <Vet/>
-            <ArticlesSection/>
-            <BrandsSection/>
-            <Textinfo/>
-            <Footer/>
-        </div>
+        <Routes>
+            <Route path="/" element={<MainLayout />}>
+                <Route index element={<Home />} />
+            </Route>
+        </Routes>
     );
 }
 
